bf79cf0d0386f6bd5606d7598004881aac7e7b60
Krystalina <krystalina99@gmail.com>
2021-09-16 16:38:30 -0700

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(firstName = "Krystalina") {
+    return `Hi, my name is ${firstName}.`;
+}
+
+function introductionWithLanguage(firstName, language = "JavaScript") {
+    return `Hi, my name is ${firstName} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(firstName, language = "JavaScript") {
+    return `Hi, my name is ${firstName} and I am learning to program in ${language}.`;
+}
+
